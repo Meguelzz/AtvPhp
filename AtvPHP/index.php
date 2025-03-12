62fa72a336f558b45bce4beda5ae22677d024ed2
Meguelzz <meguelzz.oliveira@gmail.com>
2025-03-12 20:07:01 -0300

    atividade php

---
+++ AtvPHP/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Atividade PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Exemplo Atividade PHP</h1>
+    <h2>Nesta atividade utilizaremos PHP para entregar html, javascript e css</h2>
+    <br>
+
+<div class='divAll'>
+    <div class='divParagrafos'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nisi sit amet nulla dictum rhoncus. Nullam et mollis nunc. Nunc enim urna, fringilla sed cursus non, molestie quis dui. Maecenas id dolor mauris. Vestibulum posuere turpis eget dapibus vehicula. Sed pretium vulputate orci, feugiat semper lacus iaculis nec. Suspendisse semper sodales purus. Etiam nec molestie lorem. Sed elit ante, convallis id convallis et, mattis eget quam.</p>
+
+        <p>Fusce vitae nibh viverra, posuere arcu in, euismod lacus. Maecenas et mattis neque. Suspendisse non aliquam sem, scelerisque porta leo. Sed pellentesque mi magna. Integer quis pulvinar tortor. Vivamus efficitur maximus dui, rutrum dapibus ligula rutrum quis. Sed a ante ultrices, rutrum mi eu, ultricies urna. Nullam at vehicula sapien. Curabitur a diam et enim consectetur ultrices. Quisque euismod euismod arcu, ac aliquam metus finibus id. Praesent posuere, ex eget iaculis bibendum, nisl arcu ultricies augue, sit amet egestas metus neque vel lorem. Aliquam dignissim metus vitae accumsan blandit.</p>
+
+        <p>Aliquam pretium commodo iaculis. Cras eget massa nibh. Integer dapibus nibh ac nunc faucibus egestas. Quisque orci lorem, porta sit amet placerat sed, viverra pulvinar magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lacinia augue erat, nec suscipit leo laoreet ac. Etiam facilisis cursus consequat. Pellentesque vel diam eget libero elementum venenatis. Duis vestibulum, ex dignissim sodales ullamcorper, ipsum ligula blandit purus, hendrerit finibus ligula erat nec erat. Proin in metus tincidunt, ornare quam id, euismod mauris. Vestibulum sit amet consequat neque. Vestibulum luctus imperdiet ex, et dapibus diam sagittis quis. Aenean pretium pellentesque mi id mollis. Vestibulum efficitur consequat mi, pulvinar condimentum augue pellentesque sed. Proin ut nisl a ipsum vulputate accumsan at ac odio. Cras cursus purus id arcu aliquet, ac faucibus mauris efficitur</p>
+    </div>
+
+    <aside>
+        <img class='img1' src='img/phplogo.png' alt='logo php' width='500'>
+        <img class='img2' src='img/htmllogo.png' alt='logo html' width='500'>
+    </aside>
+
+</div>
+<script>
+ alert('Bem-vindo!');
+</script>"
+?>
+</body>
+</html>
