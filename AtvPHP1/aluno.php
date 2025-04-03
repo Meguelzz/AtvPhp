64da6488e5bf7b05d2c4ec4169139bde39ee05d4
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-03 18:01:01 -0300

    Update aluno.php

--- AtvPHP1/aluno.php
+++ AtvPHP1/aluno.php
@@ -1,8 +1,8 @@
 <?php
 class aluno extends pessoa {
-function __construct($nome, $idade){
-    parent::__construct($nome,$idade);}
- function falar(){
+function __construct(nome, idade){
+    parent::__construct(nome,idade);}
+function falar(){
     echo "perguntando...";}
 }
 ?>
