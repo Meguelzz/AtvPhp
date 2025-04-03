b8ef3447c43150a4563ee8680de31d5200b1714b
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-03 18:02:09 -0300

    Update professor.php

--- AtvPHP1/professor.php
+++ AtvPHP1/professor.php
@@ -1,9 +1,9 @@
 <?php
 class professor extends pessoa {
-    function __construct($nome, $idade){
-        parent::__construct($nome,$idade);
-    }
-     function falar(){
+    function __construct(nome, idade){
+        parent::__construct(nome,idade);}
+
+    function falar(){
         echo "dando aula...";}
 }
 ?>
