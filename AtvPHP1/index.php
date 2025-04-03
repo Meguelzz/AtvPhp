fbc5fa4c362f7b5c51f85ea090f3ed25cdd43332
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-03 18:01:22 -0300

    Update index.php

--- AtvPHP1/index.php
+++ AtvPHP1/index.php
@@ -3,24 +3,24 @@ require_once "pessoa.php";
 require_once "professor.php";
 require_once "aluno.php";
 
- $pessoa = new pessoa("Pedro", "19");
+pessoa = new pessoa("Pedro", "19");
 
- echo $pessoa->getNome() . "<br>";
- echo $pessoa->getIdade() . "<br>";
- echo $pessoa->falar() . "<br>" . "<br>";
+echo pessoa->getNome() . "<br>";
+echo pessoa->getIdade() . "<br>";
+echo pessoa->falar() . "<br>" . "<br>";
 
- 
 
- $aluno = new aluno ("Lucas", "20");
 
- echo $aluno->getNome() . "<br>";
- echo $aluno->getIdade() . "<br>";
- echo $aluno->falar() . "<br>" . "<br>";
+aluno = new aluno ("Lucas", "20");
 
- 
- $professor = new professor ("Victor", "19");
+echo aluno->getNome() . "<br>";
+echo aluno->getIdade() . "<br>";
+echo aluno->falar() . "<br>" . "<br>";
 
- echo $professor->getNome() . "<br>";
- echo $professor->getIdade() . "<br>";
- echo $professor->falar();
+
+professor = new professor ("Victor", "19");
+
+echo professor->getNome() . "<br>";
+echo professor->getIdade() . "<br>";
+echo professor->falar();
 ?>
